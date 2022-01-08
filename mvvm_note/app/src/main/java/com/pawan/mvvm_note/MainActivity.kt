31f2cc697ae56b4c7560f71ced56ff82b7113167
Pawan Acharya <pawanacharya070@gmail.com>
2022-01-08 13:08:04 +0545

    init

---
+++ mvvm_note/app/src/main/java/com/pawan/mvvm_note/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.pawan.mvvm_note
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
